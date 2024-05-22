655701865dd18879bdb8369c3ed5380327600101
JeremieAgee <you@example.com>
2024-05-21 20:13:47 -0500

    created functions for chunkArray, findUnique, and exported them

--- modules/arrayUtils.js
+++ modules/arrayUtils.js
@@ -1,1 +1,11 @@
 const _ = require("lodash");
+function chunkArray(arr, size){
+    return _.chunk(arr, size);
+}
+function findUniqueElements(arr){
+    return _.uniq(arr);
+}
+module.exports = {
+    chunkArray,
+    findUniqueElements
+}
