227939e68bbe0a6f66396cdf5744a5d7a60c1104
JeremieAgee <you@example.com>
2024-05-21 20:22:19 -0500

    created object functions and exported them

--- modules/objectUtils.js
+++ modules/objectUtils.js
@@ -1,1 +1,12 @@
 const _ = require("lodash");
+function deepCloneObject(obj){
+    return _.defaultsDeep(obj);
+}
+function mergeObjects(obj1, obj2){
+    return _.merge(obj1, obj2);
+}
+
+module.exports = {
+    deepCloneObject,
+    mergeObjects
+}
